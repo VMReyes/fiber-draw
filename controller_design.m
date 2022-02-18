118bac2be3871b78fe0d07556a64fc02a8f402e4
George Chen <georgechen1999@gmail.com>
2022-02-18 08:26:39 -0500

    iterate all files with jupyter-like matlab script

--- controller_design.m
+++ controller_design.m
@@ -33,7 +33,7 @@ PrefltLEN = 1;
 
 % turn it into a train / test array
 [XTrainTranspose, YTrainTranspose] = stl_prep_training_data(BatchInfo, ...
-    STRDEF, x_columns, y_columns, fltLEN, PrefltLEN, bPlot);
+    STRDEF, x_columns, y_columns, fltLEN, PrefltLEN, bPlot, 0);
 
 
 %% visualize data
@@ -65,84 +65,64 @@ for i = 1:length(XTrainTranspose)
     cd ..;
 end
 
-%% From X Y ONEfile, Kt Controller (WRONGLY SINGLE SUBBATCH, DO NOT RUN)
-% choose model
+%% Choose model / order (BY SINGLE SUBBATCH)
 
-% cd 'C:\Users\georg\Desktop\MS Research\fiber-draw'
-% 
-% for i = 1:1 %length(XTrainTranspose)
-%     xs = cell2mat(XTrainTranspose(i));
-%     ys = cell2mat(YTrainTranspose(i));
-%     capstan_speed = xs(1,:); furnace_power = xs(2,:); preform_speed = xs(3,:);
-%     bfd = ys(1,:); tension = ys(2,:);
-% 
-%     % experiment with [1/2 2 1/2 1]
-% %     sys_kt = armax(iddata_tension_to_power,  [2 2 2 1]);
-% %     sys_kt = bj(iddata_tension_to_power,  [2 2 2 2 1]);
-% %     sys_kt = oe(iddata_tension_to_power,  [2 2 1]);
-% 
-%     dt = 1;
-%     iddata_tension_to_power     = iddata(furnace_power', tension', dt);
-%     iddata_bfd_to_capstan_speed = iddata(capstan_speed', bfd',     dt);
-%     
-%     fig = figure;
-%     if exist("_oe") == 7
-%         rmdir("C:\Users\georg\Desktop\MS Research\fiber-draw\_oe", 's');
-%     end
-%     mkdir('_oe');
-%     for a=1:4 for b = 1:4 %for c = 1:4 for d = 1:4
-% %     sys_kt = armax(iddata_tension_to_power,  [a b c 1]);
-% %     sys_kt = bj(iddata_tension_to_power,  [a b c d 1]);
-%     sys_kt = oe(iddata_bfd_to_capstan_speed,  [a b 1]);
-%     compare(iddata_bfd_to_capstan_speed, sys_kt);
-% %     title(sprintf('%d%d%d%d', a,b,c,d))
-%     title(sprintf('%d%d', a,b))
-%     [y_hat, fit, x0] = compare(iddata_bfd_to_capstan_speed, sys_kt);
-% %     [a b c d fit]
-%     [a b fit]
-%     if abs(fit) > 80
-%         cd('_oe');
-% %         saveas(fig, sprintf('%d%d%d%d',a,b,c,d),'png');
-%         saveas(fig, sprintf('%d%d',a,b),'png');
-%         cd ..;
-%     end
-%     end; end %; end; end
-% 
-% 
-% %     plot(whatsthis)
-% %     figure; hold on; 
-% %     yyaxis right; plot(furnace_power); ylabel('Furnace Power')
-% %     yyaxis left; plot(tension); ylabel('Tension'); 
-% %     legend({'tension','furnace power'})
-%     
-%     % back out PID gains
-% 
-%     
-% end
-% disp('Done!')
+cd 'C:\Users\georg\Desktop\MS Research\fiber-draw'
 
-%% analysis (WRONGLY SINGLE SUBBATCH, DO NOT RUN)
-% for i = 1:length(XTrainTranspose)
-%     xs = cell2mat(XTrainTranspose(i)); 
-%     ys = cell2mat(YTrainTranspose(i)); 
-%     capstan_speed = xs(1,:); furnace_power = xs(2,:); preform_speed = xs(3,:);
-%     bfd = ys(1,:); tension = ys(2,:);
-%     dt = 1;
+fig = figure;
+set(gcf, 'Position',[133.8000 95.4000 1.2952e+03 658.4000])
+
+if exist("_oe") == 7
+    rmdir("C:\Users\georg\Desktop\MS Research\fiber-draw\_oe", 's');
+end
+mkdir('_oe');
+
+max_order = 8;
+num_params = 2;
+fit_matrix_bool = zeros(length(XTrainTranspose), max_order^num_params);
+fit_matrix = zeros(length(XTrainTranspose), max_order^num_params);
+
+for i = 1:length(XTrainTranspose)
+    xs = cell2mat(XTrainTranspose(i));
+    ys = cell2mat(YTrainTranspose(i));
+    capstan_speed = xs(1,:); furnace_power = xs(2,:); preform_speed = xs(3,:);
+    bfd = ys(1,:); tension = ys(2,:);
+
+%     sys_kt = armax(iddata_tension_to_power,  [2 2 2 1]);
+%     sys_kt = bj(iddata_tension_to_power,  [2 2 2 2 1]);
+%     sys_kt = oe(iddata_tension_to_power,  [2 2 1]);
+
+    dt = 1;
 %     iddata_tension_to_power     = iddata(furnace_power', tension', dt);
-%     iddata_bfd_to_capstan_speed = iddata(capstan_speed', bfd',     dt);
-%     
-%     % replace for different graph
-%     sys_kt = oe(iddata_bfd_to_capstan_speed,  [3 4 1]); 
-%     [sys_kt.A, sys_kt.B sys_kt.C sys_kt.D sys_kt.F]
-% 
-%     [y_hat, fit, x0] = compare(iddata_bfd_to_capstan_speed, sys_kt);
-% %     figure(1); plot(capstan_speed); hold on; plot(y_hat, 'r'); hold off;
-% %     stl_plottrainingresults_FFTPSD_function(capstan_speed, capstan_speed, y_hat.OutputData, y_hat.OutputData);
-% 
-% %     figure(1); plot(furnace_power); hold on; plot(y_hat, 'r'); hold off;
-% %     stl_plottrainingresults_FFTPSD_function(furnace_power, furnace_power, y_hat.OutputData, y_hat.OutputData);
-%     pause(1)
-% end
+    iddata_bfd_to_capstan_speed = iddata(capstan_speed', bfd',     dt);
+    
+    for a=2:max_order for b = 3:max_order %for c = 1:4 for d = 1:4
+%     sys_kt = armax(iddata_tension_to_power,  [a b c 1]);
+%     sys_kt = bj(iddata_tension_to_power,  [a b c d 1]);
+    sys_kt = oe(iddata_bfd_to_capstan_speed,  [a b 1]);   
+    [y_hat, fit, x0] = compare(iddata_bfd_to_capstan_speed, sys_kt);
+%     [a b c d fit]
+    disp([i a b fit])
+
+    if (30 < abs(fit) && abs(fit) < 200)
+        fit_matrix_bool(i, (a-1)*max_order+b) = 1;
+        fit_matrix(i, (a-1)*max_order+b) = abs(fit);
+        subplot(2,1,1); compare(iddata_bfd_to_capstan_speed, sys_kt);
+        title(sprintf('%d, %d%d', i,a,b)); 
+        ax = gca; ax.Legend.Location = 'southeast';
+        
+        subplot(2,1,2);
+        plot_fft(capstan_speed, capstan_speed, y_hat.OutputData, y_hat.OutputData)
+
+        cd('_oe');
+%         saveas(fig, sprintf('%d%d%d%d',a,b,c,d),'png');
+        saveas(fig, sprintf('%d,%d%d',i,a,b),'png');
+        cd ..;
+    end
+    end; end %; end; end
+
+end
+disp('Done!')
 
 %% analysis (COEFFS BY SUBBATCH MODALITY)
 as = zeros(1,length(XTrainTranspose));
@@ -245,18 +225,65 @@ figure; plot(y_concat)
 iddata_bfd_to_capstan_speed_total = iddata(y_concat', u_concat', dt);
 
 %% part 2
-for a = 1:4 for b = 1:4 for c = 1:4
-if (a == 2 && b > 3)
-    continue;
+cd 'C:\Users\georg\Desktop\MS Research\fiber-draw'
+fig = figure; 
+
+folder_name = '_fit';
+if exist(folder_name) == 7
+    rmdir("C:\Users\georg\Desktop\MS Research\fiber-draw\"+folder_name, 's');
 end
-% sys_kd = oe(iddata_bfd_to_capstan_speed_total, [a b 1]) % 4 1 1 
-sys_kd = armax(iddata_bfd_to_capstan_speed_total,  [a b c 1]);
+mkdir(folder_name);
+for a = 1:4 for b = 1:4 % for c = 1:4
+% if (a == 2 && b > 3)
+%     continue;
+% end
+
+% a = 4;
+% b = 1;
+% oeopt = oeOptions;
+% oeopt.InitialCondition = 'estimate'; % 'backcast';
+% oeopt.SearchOptions.MaxIterations = 20;
+% oeopt.Display = 'off';
+sys_kd = oe(iddata_bfd_to_capstan_speed_total, [a b 1]) % 4 1 1 
+% sys_kd = armax(iddata_bfd_to_capstan_speed_total,  [a b c 1]);
 [y_hat, fit, x0] = compare(iddata_bfd_to_capstan_speed_total, sys_kd);
 figure(1); plot(y_concat); hold on; plot(y_hat, 'r'); hold off;
 stl_plottrainingresults_FFTPSD_function(y_concat, y_concat, y_hat.OutputData, y_hat.OutputData);
-[a b c fit]
+[a b fit]
+% [a b c fit]
 
-figure; histogram(y_concat); hold on; histogram(y_hat.OutputData); hold off;
+figure(2); histogram(y_concat); hold on; histogram(y_hat.OutputData); hold off;
 xlim([2000 2600])
-end; end; end
+
+cd(folder_name);
+saveas(fig, num2str(i), 'png');
+cd ..;
+end; end %; end
 disp('Done!')
+
+
+%% helper function
+function [f] = plot_fft(Y, Y_filt, Ypredict, Ypredict_filt)
+    %take the FFT
+    FY = fft(Y);
+    FY_s = fftshift(FY);
+    FY_filt = fft(Y_filt);
+    %take the FFT
+    FYpredict = fft(Ypredict);
+    FYpredict_s = fftshift(FYpredict);
+    FYpredict_filt = fft(Ypredict_filt);
+
+    %the radial frequencies
+    frq_discretes = 2/length(Y).*([0:(length(Y)-1)]-length(Y)/2);
+
+    plot(frq_discretes,log10(abs(FY_s).^2),'k');
+    hold on
+    plot(frq_discretes,log10(abs(FYpredict_s).^2),'r'); 
+    hold off
+    
+    ylabel('Log Magnitude')
+    xlabel('Frequency')
+    legend('Data', 'Prediction')
+        axis([0 1 -.1  max([   max(log10(abs(FY_s).^2))   max(log10(abs(FYpredict_s).^2))   ])   ])
+    title('Power Spectrum');
+end
