24de406e4364eebb3f358193dd351133840ae582
Victor Reyes <vmrespinoza@gmail.com>
2022-03-01 17:13:42 -0500

    added deep lstm to architecture comparison

--- architecture_experiment.m
+++ architecture_experiment.m
@@ -1,22 +1,25 @@
 load("alldatatrain\all_data_processed_4in_1out_yremove125.mat", "Xdata", "Ydata", ...
-     "filenames")   
+     "x_train", "y_train", "x_test", "y_test", "filenames")   
 
-% combine data
-combined_Xdata = cat(2, Xdata{:});
-combined_Ydata = cat(2, Ydata{:});
+% % combine data
+% combined_Xdata = cat(2, Xdata{:});
+% combined_Ydata = cat(2, Ydata{:});
+% 
+% [~, num_batches] = size(combined_Xdata);
+% [train_ind, test_ind] = dividerand(num_batches, 0.9, 0.1, 0.0);
+% 
+% x_train = combined_Xdata(train_ind);
+% y_train = combined_Ydata(train_ind);
+% x_test = combined_Xdata(test_ind);
+% y_test = combined_Ydata(test_ind);
 
-[~, num_batches] = size(combined_Xdata);
-[train_ind, test_ind] = dividerand(num_batches, 0.9, 0.1, 0.0);
-
-x_train = combined_Xdata(train_ind);
-y_train = combined_Ydata(train_ind);
-x_test = combined_Xdata(test_ind);
-y_test = combined_Ydata(test_ind);
-
-% for every architecture, train);
+% for every architecture, train;
 
 simple_lstm = create_simple_lstm(x_train, y_train);
 [simple_lstm, simple_lstm_info] = train_lstm(simple_lstm, x_train, y_train, x_test, y_test, 64);
+
+deep_lstm = create_deep_lstm(x_train, y_train);
+[deep_lstm, deep_lstm_info] = train_lstm(deep_lstm, x_train, y_train, x_test, y_test, 16);
 
 simple_bilstm = create_simple_bilstm(x_train, y_train);
 [simple_bilstm, simple_bilstm_info] = train_lstm(simple_bilstm, x_train, y_train, x_test, y_test, 16);
@@ -25,8 +28,6 @@ side_by_side_3_lstm = create_3_side_by_side_lstm(x_train, y_train);
 [side_by_side_3_lstm, side_by_side_3_lstm_info] = train_lstm(side_by_side_3_lstm, x_train, y_train, x_test, y_test, 32);
 
 
-save("alldatatrain\architecture_models.mat", "simple_lstm", "simple_lstm_info", ...
-     "simple_bilstm", "simple_bilstm_info", "side_by_side_3_lstm", ...
+save("results/architecture_experiment.mat", "simple_lstm", "simple_lstm_info", ...
+     "deep_lstm", "deep_lstm_info", "simple_bilstm", "simple_bilstm_info", "side_by_side_3_lstm", ...
      "side_by_side_3_lstm_info");
-
-visualize_model(x_test, y_test, simple_bilstm);
--- stl_plot/visualize_model.m
+++ stl_plot/visualize_model.m
@@ -3,15 +3,21 @@ function visualize_model(x_test, y_test, net)
 reset_net = net.resetState();
 y_test_net_pred = reset_net.predict(x_test);
 
-tiledlayout(2,1);
+tiledlayout(3,2);
+for i = 1:3
 
-nexttile;
-plot(y_test{1}); hold on;
-plot(y_test_net_pred{1}); hold off;
-title(net_name + ' - Actual Vs. Predicted BFD on Testing Data');
-ylim([-0.2 0.2]);
+    nexttile;
+    plot(y_test{i}); hold on;
+    plot(y_test_net_pred{i}); hold off;
+    title('Actual Vs. Predicted BFD on Testing Data');
+    ylim([-0.2 0.2]);
+    
+    nexttile;
+    plot(smoothdata(y_test{i}, 'movmean', 20)); hold on;
+    plot(y_test_net_pred{i}); hold off;
+    title('Actual (smoothed) Vs. Predicted BFD on Testing Data');
+    ylim([-0.075 0.075]);
+end
 
-nexttile;
-plot(abs(y_test{1} - y_test_net_pred{1}));
-title(net_name + ' - Actual minus Predicted BFD on Testing Data');
-ylim([0 0.2]);
+stl_plottrainingresults_FFTPSD_function(y_test{1}, y_test{1}, ...
+    y_test_net_pred{1}, y_test_net_pred{1})
