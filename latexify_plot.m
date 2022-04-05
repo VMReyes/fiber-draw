b1151607fdb7558066a6dcdf4146d81b623067a9
George Chen <georgechen1999@gmail.com>
2022-04-05 16:52:39 -0400

    working sim bode plot code

---
+++ latexify_plot.m
@@ -0,0 +1,6 @@
+set(gcf, 'Color', 'white');
+set(groot, 'defaultAxesTickLabelInterpreter','latex'); 
+set(groot, 'defaultLegendInterpreter','latex');
+set(groot, 'defaultTextInterpreter','latex');
+ax = gca;
+ax.Title.Interpreter = 'latex';
--- make_some_figures.m
+++ make_some_figures.m
@@ -99,26 +99,47 @@ title('Example Batch of BFD Data'); xlabel('Samples'); ylabel('Bare Fiber Diamet
 end
 
 disp('Done!')
-%% generate simulated / empirical bode 
-clc; clear;
+%% generate simulated bode (typical runtime: 2 hrs)
+clc; clear; close all;
 load("run_results\architecture_experiment.mat");
 load("alldatatrain\all_data_processed_4in_1out_yremove125.mat");
+curr_path = 'C:\Users\georg\Desktop\fiber-draw';
+folder_name = 'sim_bode_results';
+if exist(folder_name, 'dir') ~= 7
+    mkdir(folder_name);
+end
+
 combined_Xdata = cat(2, Xdata{:});
 
 % Xdata rows: capstan speed, furnace power, He temp, preform velocity
 
-stats_matrix = zeros(length(combined_Xdata),4);
+mean_matrix = zeros(length(combined_Xdata),4);
 for i = 1:length(combined_Xdata)
     subbatch = combined_Xdata{i};
-    stats_matrix(i,:) = [mean(subbatch(1,:)) mean(subbatch(2,:)) mean(subbatch(3,:)) mean(subbatch(4,:))];
+    mean_matrix(i,:) = [mean(subbatch(1,:)) mean(subbatch(2,:)) mean(subbatch(3,:)) mean(subbatch(4,:))];
 end
 % figure; plot(stats_matrix)
 
-% alist = zeros(1, length(combined_Xdata));
-% for i = 1:length(combined_Xdata)
-%     alist(i) = (max(combined_Xdata{i}(1,:)) - min(combined_Xdata{i}(1,:)));
-% end
-% figure; histogram(alist,200,"BinLimits",[0 2700])
+% visualize the distribution of ranges (max-min) of signal
+for row = 1:4
+    ranges = zeros(1, length(combined_Xdata));
+    for i = 1:length(combined_Xdata)
+        ranges(i) = (max(combined_Xdata{i}(row,:)) - min(combined_Xdata{i}(row,:)));
+    end
+    figure; 
+    
+    switch row
+        case 1
+            histogram(ranges,200,"BinLimits",[0 2700]); xline(700, 'LineWidth',2); % 1st row
+        case 2
+            histogram(ranges,100,"BinLimits",[0 15]); xline(10, 'LineWidth',2); % 2nd row
+        case 3
+            histogram(ranges,100,"BinLimits",[0 5]); xline(3, 'LineWidth',2); % 3rd row
+        case 4
+            histogram(ranges,100,"BinLimits",[0 35]); xline(15, 'LineWidth',2) % 4th row
+    end
+    latexify_plot;
+end
 
 combined_Xdata = cat(2, Xdata{:});
 combined_Ydata = cat(2, Ydata{:});
@@ -127,14 +148,21 @@ sampling_freq = 2; % Hz
 nyq_freq = sampling_freq * 2*3 / 2;
 avg_capstan_speed = 2500; % 2500 or 2685
 subbatch_length = 8000;
-t = 1:subbatch_length; % subbatch length
+t = 1:subbatch_length; 
 
 all_w = logspace(-3,log10(nyq_freq),100);
-% all_A = 5:5:700; % for capstan speed
 all_max_A = [700 10 3 20]; % 1st, 2nd, 3rd, 4th row
+fit_opt = fitoptions('Method','LinearLeastSquares' , 'Robust', 'Bisquare');
 
-for row = 2:length(all_max_A) 
-    all_A = 0:0.1:all_max_A(row);
+bPlot = 0;
+
+for row = 1:4
+    if row == 1
+        all_A = 5:5:all_max_A(row);
+    else
+        all_A = 0:0.1:all_max_A(row);
+    end
+
     bode_p2p = zeros(length(all_A), length(all_w));
     bode_fit_obj = cell(length(all_A), length(all_w));
     
@@ -142,93 +170,119 @@ for row = 2:length(all_max_A)
         for j = 1:length(all_w)
             
             A = all_A(i); w = all_w(j);
-    
-    %         sine_wave = (avg_capstan_speed-A/2) * sin(w*t);
-        
-            % modify 1st, 2nd, 3rd, 4th line to be sine wave
-    %         new_Xdata = [ones(1,subbatch_length)*avg_capstan_speed + sine_wave; 
-    %                      ones(1,subbatch_length)*median(stats_matrix(:,2)); 
-    %                      ones(1,subbatch_length)*median(stats_matrix(:,3)); 
-    %                      ones(1,subbatch_length)*median(stats_matrix(:,4))];
-            
             sine_wave = A * sin(w*t);
+            
+            % add sine wave to 1st, 2nd, 3rd, 4th row
             new_Xdata = [ones(1,subbatch_length)*avg_capstan_speed; 
-                         ones(1,subbatch_length)*median(stats_matrix(:,2));
-                         ones(1,subbatch_length)*median(stats_matrix(:,3)); 
-                         ones(1,subbatch_length)*median(stats_matrix(:,4))];
+                         ones(1,subbatch_length)*median(mean_matrix(:,2));
+                         ones(1,subbatch_length)*median(mean_matrix(:,3)); 
+                         ones(1,subbatch_length)*median(mean_matrix(:,4))];
 
             new_Xdata(row,:) = new_Xdata(row,:) + sine_wave;
     
             % see, it's similar to input!
-            % figure(1); plot(combined_Xdata{1}'); hold on; plot(new_Xdata{1}', 'k'); hold off;
+%             figure(6); plot(combined_Xdata{1}'); hold on; plot(new_Xdata', 'k'); hold off;
+
             net = deep_lstm.resetState();
             y_pred = net.predict(new_Xdata, "MiniBatchSize", 1);
     
             eqn_str = sprintf('a*sin(%f*x+phi)+c',w);
             ft = fittype(eqn_str, 'independent', 'x', 'dependent', 'y');
-            fit_opt = fitoptions('Method','LinearLeastSquares' , 'Robust', 'Bisquare');
             [fit_obj, goodness_info] = fit(t', y_pred', ft);
     
             % identify peaks
             peaks_ind = islocalmax(y_pred); troughs_ind = islocalmin(y_pred);
             all_peaks = y_pred(peaks_ind);
-            all_peaks = all_peaks(all_peaks > (fit_obj.c + abs(fit_obj.a)));
+            all_peaks = all_peaks(all_peaks > (fit_obj.c + abs(fit_obj.a)/2));
             all_troughs = y_pred(troughs_ind);
-            all_troughs = all_troughs(all_troughs < (fit_obj.c)); 
-            A_p2p = (mean(all_peaks(2:end)) - mean(all_troughs(2:end)))/2;
-%             peaks = zeros(1,length(y_pred)); troughs = zeros(1,length(y_pred));
-%             for k = 1:length(peaks_ind)
-%                 if peaks_ind(k) && y_pred(k) > (fit_obj.c + abs(fit_obj.a))
-%                     peaks(k) = y_pred(k);
-%                 else
-%                     peaks(k) = nan;
+            all_troughs = all_troughs(all_troughs < (fit_obj.c - abs(fit_obj.a)/2)); 
+            A_p2p = (median(all_peaks) - median(all_troughs))/2;
+            bode_fit_obj{i,j} = fit_obj; bode_p2p(i,j) = A_p2p;
+
+%             if bPlot
+%                 peaks = zeros(1,length(y_pred)); troughs = zeros(1,length(y_pred));
+%                 for k = 1:length(peaks_ind)
+%                     if peaks_ind(k) && y_pred(k) > (fit_obj.c + abs(fit_obj.a)/2)
+%                         peaks(k) = y_pred(k);
+%                     else
+%                         peaks(k) = nan;
+%                     end
 %                 end
-%             end
-%             for k = 1:length(troughs_ind)
-%                 if troughs_ind(k) && y_pred(k) < (fit_obj.c)
-%                     troughs(k) = y_pred(k);
-%                 else
-%                     troughs(k) = nan;
+%                 for k = 1:length(troughs_ind)
+%                     if troughs_ind(k) && y_pred(k) < (fit_obj.c + abs(fit_obj.a)/2)
+%                         troughs(k) = y_pred(k);
+%                     else
+%                         troughs(k) = nan;
+%                     end
 %                 end
+%     
+%                 figure(7); set(gcf, 'Position', [31 500 1815 478]);
+%                 plot(fit_obj, t, y_pred, '-'); hold on;
+%                 plot(ones(1, length(subbatch))*median(all_peaks),'LineWidth',3); 
+%                 plot(ones(1, length(subbatch))*median(all_troughs),'LineWidth',3);
+%                 
+%                 plot(t, peaks, '.', 'MarkerSize', 20);
+%                 plot(t, troughs, '.', 'MarkerSize', 20);
+%                 hold off; 
+%                 if (j > 50) xlim([0 1e2]); end
+% %                 saveas(gcf, sprintf('%s\\sim_bode_plots\\%d,%d,%d', curr_path, row, i, j),'fig');
+% %                 saveas(gcf, sprintf('%s\\sim_bode_plots\\%d,%d,%d', curr_path, row, i, j),'png');
 %             end
 
-                    
-    
-            figure(2); 
-            plot(fit_obj, t, y_pred, '-'); hold on;
-            plot(ones(1, length(subbatch))*max(all_peaks(2:end))); 
-            plot(ones(1, length(subbatch))*min(all_troughs(2:end)));
-            
-%             plot(t, peaks, '.', 'MarkerSize', 20);
-%             plot(t, troughs, '.', 'MarkerSize', 20);
-            hold off; pause(2)
-
-%             xlim([0 1e2]); 
             fprintf('row: %d\t i: %d/%d\t j:%d/%d\n', row, i, length(all_A), j, length(all_w))
         end
     end
-    save(sprintf('simulated_bode_%d.mat',row),"bode_fit_obj",'bode_p2p');
+    if (i == length(all_A) && j == length(all_w))
+        save(sprintf('%s\\%s\\simulated_bode_%d.mat', curr_path, folder_name, row),"bode_fit_obj",'bode_p2p');
+    end
 end
 %% plot simulated bode
 clc; close all;
-load simulated_bode.mat
+
+row = 4; % 1, 2, 3, 4
+
+load(sprintf('sim_bode_results\\simulated_bode_%d.mat',row));
+
+sampling_freq = 2; % Hz
+nyq_freq = sampling_freq * 2*3 / 2;
+avg_capstan_speed = 2500; % 2500 or 2685
+subbatch_length = 8000;
+t = 1:subbatch_length; 
+all_w = logspace(-3,log10(nyq_freq),100);
+all_max_A = [700 10 3 20]; % 1st, 2nd, 3rd, 4th row
+if row == 1
+    all_A = 5:5:all_max_A(row);
+else
+    all_A = 0:0.1:all_max_A(row);
+end
 
 figure; axes('XScale', 'log', 'YScale', 'log')
 hold on;
-for A = 1:size(bode_fit_obj,1)
-    row = zeros(1,size(bode_fit_obj,2));
-    for i = 1:length(row)
-        row(i) = abs(bode_fit_obj{A,i}.a)/all_A(A);
+for A = 2:size(bode_fit_obj,1)
+    if A~=12
+        row = zeros(1,size(bode_fit_obj,2));
+        for i = 1:length(row)
+            row(i) = abs(bode_fit_obj{A,i}.a)/all_A(A);
+        end
+        plot(all_w, row)
     end
-    plot(all_w, row)
+    pause(0.5)
 end
+xline(nyq_freq);
+xlabel('Frequency (rad/s)'); ylabel('Gain');
+title('Simulated Bode Plot of Fiber Drawing Plant')
+grid on; grid minor;
+xlim([1e-3 10])
+latexify_plot;
 
 figure; axes('XScale', 'log', 'YScale', 'log');
 hold on;
-for A = 1:10 %1:size(bode_p2p,1)
+for A = 2:size(bode_p2p,1)
     plot(all_w, bode_p2p(A,:)./all_A(A))
 end
 xline(nyq_freq);
 xlabel('Frequency (rad/s)'); ylabel('Gain');
 title('Simulated Bode Plot of Fiber Drawing Plant')
-xlim([1e-3 10]) %band-aid fix, need fix p2p calc & resample
+grid on; grid minor;
+xlim([1e-3 10]) 
+latexify_plot;
