49d80f113fe89433f2f5140ef3722da2db949791
Victor Reyes <vmrespinoza@gmail.com>
2022-04-21 14:18:17 -0400

    closed loop simulation in progress

--- closed_loop_simulation.m
+++ closed_loop_simulation.m
@@ -1,5 +1,8 @@
+clc; clear; close all; 
+
 load("run_results\architecture_experiment.mat", "deep_lstm");
 load("alldatatrain\all_data_processed_4in_1out_yremove125.mat", "x_test", "y_test");
+load("sys_kd_total_oe.mat", "m", "sys_kd_total")
 
 x_sample = x_test{1};
 y_sample = y_test{1};
@@ -7,7 +10,7 @@ net = deep_lstm;
 
 %simulate with known controller and learned system model
 bfd_setpoint = 125; % setpoint for controller
-% TODO(vmreyes): time_to_steady_state = 0; % timesteps to replay before simulating controller
+time_to_steady_state = 5; % timesteps to replay before simulating controller
 curr_bfd = 0;
 
 uarray = [];
@@ -15,19 +18,27 @@ earray = [];
 YoutSymA_NN = zeros(size(8000));
 
 gainP = 1.0;
-for ii = 1:7999
-    if ii == 1
-        [net, curr_bfd] = predictAndUpdateState(net,x_sample(:,1));
-        curr_bfd = curr_bfd + 125;
-        YoutSymA_NN(1) = curr_bfd;
-    end
+
+% initialize controller state
+for ii = 1:time_to_steady_state
+    [net, curr_bfd] = predictAndUpdateState(net,x_sample(:,ii));
+    curr_bfd = curr_bfd + 125;
+    YoutSymA_NN(ii) = curr_bfd;
+    uarray{end+1} = x_sample(1:1, ii);
+end
+
+for ii = 1:8000-time_to_steady_state
     e = 125 - curr_bfd;
     %e = e*gainP;
 
     %simulate with known controller
 
-    %Un00 = b0/a0*e; 
-    Un00 = gainP*e; 
+    %Un00 = b0/a0*e;
+    
+    %Un00 = gainP*e; 
+
+    Un00 = e * sys_kd_total.B(4) * uarray{end-2} / ...
+        (sys_kd_total.F(1) + sys_kd_total.F(2)*uarray{end}+sys_kd_total.F(3)*uarray{end-1}+sys_kd_total.F(4)*uarray{end-2}+sys_kd_total.F(5)*uarray{end-3});
 
     earray{end+1} = e;
     uarray{end+1} = Un00;
@@ -41,7 +52,7 @@ for ii = 1:7999
     %[net,Ynp1] = predictAndUpdateState(net,e);%why not Un00?
     % FIXED ABOVE DO NOT SCALE WITH b0/a0 - that was just in the original synthetic generation / simulation
     %
-    YoutSymA_NN(ii+1) = curr_bfd;
+    YoutSymA_NN(ii+time_to_steady_state) = curr_bfd;
 end
 
 figure(1)
@@ -55,4 +66,5 @@ subplot(2,1,2)
 plot(cell2mat(uarray))
 xlabel('T'); ylabel('Controller Output');
 title('Controller Outputs')
+latexify_plot;
 
