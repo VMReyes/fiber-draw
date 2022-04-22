e7027c774db9ffb365924ed7f8565688e0544602
George Chen <georgechen1999@gmail.com>
2022-04-22 00:42:54 -0400

    closed-loop kinda works?

--- closed_loop_simulation.m
+++ closed_loop_simulation.m
@@ -1,33 +1,37 @@
+%% Init
 clc; clear; close all; 
 
 load("run_results\architecture_experiment.mat", "deep_lstm");
 load("alldatatrain\all_data_processed_4in_1out_yremove125.mat", "x_test", "y_test");
 load("sys_kd_total_armax.mat", "sys_kd_total")
-% load("all_file_data.mat") % TODO: may not run
+load("sys_kt_total_armax.mat", "sys_kt_total")
 
 folder_name = "_closed_loop_sim";
 if exist(folder_name, 'dir') ~= 7
     mkdir(folder_name);
 end
 
-for i = 1:length(x_test)
-x_sample = x_test{30}; %44
-y_sample = y_test{30}; % 44
-net = deep_lstm;
-
 % simulate with known controller and learned system model
 bfd_setpoint = 125; % setpoint for controller
-time_to_steady_state = 30; % timesteps to replay before simulating controller
+time_to_steady_state = 9; % timesteps to replay before simulating controller
 curr_bfd_error = 0;
-capstan_speed_prev = x_sample(4,1); % set to first value
-T = length(y_sample); % usually 8000 with some exceptions;
 dt = 0.5;
+net = deep_lstm;
 
-uarray = zeros(1, T);
-earray = zeros(1, T);
-nn_output = zeros(1, T);
+A_kd = sys_kd_total.A;
+B_kd = sys_kd_total.B; 
+C_kd = sys_kd_total.C;
+noise_variance_kd = sys_kd_total.NoiseVariance;
+
+A_kt = sys_kt_total.A;
+B_kt = sys_kt_total.B; 
+C_kt = sys_kt_total.C;
+noise_variance_kt = sys_kt_total.NoiseVariance;
+
 orders_kd_armax = [4 3 1 3];
+nk_kd = 3;
 orders_kt_armax = [3 2 2 3];
+nk_kt = 3;
 
 % Preform Velocity Lookup Table
 slope = [0 0.01 0.2 0.3 0.5 0.8 1 1.5 2 3 4];
@@ -54,69 +58,112 @@ lookup_table_130 = @(slope_input) max(F130(slope_input), -10);
 
 disp("Init Complete!")
 
-%% verify Kd works
-% % for i = 1:length(x_test)
-%     test_x_input = x_test{i}; % [44 30 17 33 29]
+%% verify sys_kd_total works
+% for i = 1:length(x_test)
+%     test_x_input = x_test{i};
 %     test_y_input = y_test{i};
 %     test_capstan_speed = test_x_input(1,:);
-%     test_bfd = test_y_input;
-%     iddata_bfd_to_capstan_speed = iddata(test_capstan_speed', test_bfd'-125,     dt);
-%     sys_kd = armax(iddata_bfd_to_capstan_speed, orders_kd_armax);
-%     compare(iddata_bfd_to_capstan_speed, sys_kd);
-% %     disp(i)
-% % end
+%     test_bfd_error = test_y_input;
+%     iddata_bfd_to_capstan_speed = iddata(test_capstan_speed', test_bfd_error',     dt);
+%     [y_hat, fit, ~] = compare(iddata_bfd_to_capstan_speed, sys_kd_total);
+%     disp([i fit]); 
+% %     figure(1); compare(iddata_bfd_to_capstan_speed, sys_kd_total); pause(1);
+% end
+
+%% verify time stepping works
+% test_x_input = x_test{16}; 
+% test_y_input = y_test{16};
+% test_capstan_speed = test_x_input(1,:);
+% test_bfd_error = test_y_input;
+% 
+% 
+% for T = 30:length(test_bfd)
+%     iddata_bfd_to_capstan_speed = iddata(test_capstan_speed(1:T)', test_bfd_error(1:T)',dt);
+%     [y_hat, fit, ~] = compare(iddata_bfd_to_capstan_speed, sys_kd_total);
+%     disp([T fit])
+%     figure(1); compare(iddata_bfd_to_capstan_speed, sys_kd_total);
+% end
+
 %% Simulate
-for t = 1:time_to_steady_state
-    [net, curr_bfd_error] = predictAndUpdateState(net, x_sample(:, t));
-    nn_output(t) = curr_bfd_error + 125;
-    uarray(t) = x_sample(1, t);
-    earray(t) = -curr_bfd_error;
-end
 
-for t = time_to_steady_state+1 : T
+for subbatch = 19 % good subbatches [16 18 19 29] [7 16 18 19 23 29 30 33 44 45]
+    x_sample = x_test{subbatch};
+    y_sample = y_test{subbatch};
 
-    %simulate with known controller
-    earray(t) = -curr_bfd_error;
+    capstan_speed_prev = x_sample(1,1); % set to first value?
+    T = length(y_sample); % usually 8000 with some exceptions;
 
-%     Kd_output = 1.0 * curr_bfd_error;
+    Kd_output_array = zeros(1, T); %uarray
+    Kd_input_array = zeros(1, T); %earray
+    Kt_input_array = zeros(1, T);
+    Kt_output_array = zeros(1, T);
+    nn_output = zeros(1, T);
+    white_noise = sqrt(noise_variance_kd) .* randn(1,T);
 
-%     Kd_output = e * sys_kd_total.B(4) * uarray(end-2) / ...
-%         (sys_kd_total.F(1) + sys_kd_total.F(2)*uarray(end)+sys_kd_total.F(3)*uarray(end-1)+sys_kd_total.F(4)*uarray(end-2)+sys_kd_total.F(5)*uarray(end-3));
+    disp("Simulation Started...")
+    for t = 1:time_to_steady_state
+        Kd_output_array(t) = x_sample(1, t);
+        capstan_speed_slope = (Kd_output_array(t) - capstan_speed_prev)/dt;
+        preform_velocity = lookup_table_100(capstan_speed_slope);
 
-    Kd_output = lsim(sys_kd_total, earray(1:t), 0:dt:dt*(t-1));
-    Kd_output = Kd_output(t);
-    
-    uarray(t) = Kd_output;
-    
-    capstan_speed_slope = (Kd_output - capstan_speed_prev)/dt;
-    preform_velocity = lookup_table_100(capstan_speed_slope);
+        [net, curr_bfd_error] = predictAndUpdateState(net, [x_sample(1:3, t); preform_velocity]);
+        nn_output(t) = curr_bfd_error + 125;
+        Kd_input_array(t) = curr_bfd_error;
+        capstan_speed_prev = x_sample(1, t);
+    end
 
-    nn_input = [Kd_output; x_sample(2:3, t); preform_velocity];
+    for t = time_to_steady_state+1 : T
 
-    [net, curr_bfd_error] = predictAndUpdateState(net, nn_input);
-    nn_output(t) = curr_bfd_error + 125;
+        % ----------- Kd: BFD error -> capstan speed -------------
+        Kd_input_array(t) = curr_bfd_error;
 
-    capstan_speed_prev = Kd_output;
-end
-disp('Simulation Done!')
+%         Kd_output = 1.0 * curr_bfd_error; % dummy test case
+% % or this:
+%         Kd_output = lsim(sys_kd_total, Kd_input_array(1:t), 0:dt:dt*(t-1));
+%         Kd_output = Kd_output(end);
+% % or this:
+        Kd_output = B_kd(4)*Kd_input_array(t-nk_kd-3) + B_kd(5)*Kd_input_array(t-nk_kd-4) + B_kd(6)*Kd_input_array(t-nk_kd-5) ...
+                    + white_noise(t) + C_kd(2)*white_noise(t-1) ...
+                    - (A_kd(2)*Kd_output_array(t-1) + A_kd(3)*Kd_output_array(t-2) + A_kd(4)*Kd_output_array(t-3) + A_kd(5)*Kd_output_array(t-4));
 
-% plots
-fig = figure(1);
-subplot(3,1,1)
-plot(nn_output)
-xlabel('t'); ylabel('Simulated BFD');
-title('Output of closed loop simulations using learned model')
+        Kd_output_array(t) = Kd_output;
 
-subplot(3,1,2)
-plot(uarray)
-xlabel('T'); ylabel('Controller Output');
-title('Controller Outputs')
+        % ----------- Lookup Table  -------------
+        capstan_speed_slope = (Kd_output - capstan_speed_prev)/dt;
+        preform_velocity = lookup_table_100(capstan_speed_slope);
 
-subplot(3,1,3)
-plot(earray)
-xlabel('T'); ylabel('Controller Input');
-title('Controller Input / BFD Error')
-latexify_plot;
+        % ----------- Kt: tension error -> furnace power  -------------
+        
 
-saveas(fig, sprintf('%s\\%s\\%i', i),'png');  
+
+        % ----------- Combine  -------------
+        nn_input = [Kd_output; x_sample(2:3, t); preform_velocity];
+%         nn_input = [x_sample(:,t)]; % dummy test case
+
+        [net, curr_bfd_error] = predictAndUpdateState(net, nn_input);
+        nn_output(t) = curr_bfd_error + 125;
+
+        capstan_speed_prev = Kd_output;
+    end
+    disp('Simulation Done!')
+
+    % plots
+    fig = figure(1);
+    subplot(3,1,1)
+    plot(nn_output)
+    xlabel('t'); ylabel('Simulated BFD');
+    title('Output of closed loop simulations using learned model')
+
+    subplot(3,1,2)
+    plot(Kd_output_array)
+    xlabel('T'); ylabel('Controller Output');
+    title('Controller Outputs')
+
+    subplot(3,1,3)
+    plot(Kd_input_array)
+    xlabel('T'); ylabel('Controller Input');
+    title('Controller Input / BFD Error')
+    latexify_plot;
+
+    saveas(fig, sprintf('%s\\%i', folder_name, subbatch),'png');
 end
